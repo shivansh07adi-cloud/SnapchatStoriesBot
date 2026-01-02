665052a29881d7230c7b70120cc75c3bb33e5c1f
shivansh07adi-cloud <shivansh07adi@gmail.com>
2026-01-02 12:39:53 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -10,10 +10,10 @@ logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 
 class ENV_VARS(object):
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
+    API_ID = int(os.environ.get("API_ID","35493276"))
+    API_HASH = os.environ.get("API_HASH","4b6b15c5aa8546bf3b9d5c6f2ddf724d")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN","8565552947:AAFfruzlvyBqgjtY5OHol7tKM9F1Z1ZvJxk")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME","YTtoAudio07Bot")
     #AUTH_USER = int(os.environ.get("AUTH_USER", 5071059420))
 
 
